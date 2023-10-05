d5729a6efb4bf868c2b445855e2cd22ea5f998ca
mchez0k <mchezok@gmail.com>
2023-10-05 17:03:39 +0300

    Добавил классы цвета, фигуры, клетки, движения фигуры и класс доски.

--- Chess/Class1.cs
+++ Chess/Class1.cs
@@ -2,6 +2,21 @@
 {
     public class Chess
     {
+        public string fen { get; private set;}
+        public Chess(string fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+        {
 
+        }
+
+        public Chess Move(string fen)
+        {
+            Chess nextChess = new Chess(fen);
+            return nextChess;
+        }
+
+        public char GetFigureAt(int x, int y)
+        {
+            return '.';
+        }
     }
 }
